07ac6ca217241655cbca15d959ffab76cb861018
Manfredas Lamsargis <manfredas.lamsargis@proton.me>
2024-03-20 15:00:27 +0200

    dev: added console app just for better testing of the client class

---
+++ backend-canvas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_canvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
