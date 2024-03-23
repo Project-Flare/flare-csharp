2df71008da732c1113fa0d90307e7a08b83fb4df
Titas Jasevičius <titjas@ktu.lt>
2024-03-23 13:20:44 +0200

    backendtests base

---
+++ Backend-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using static flare_csharp.Client;
+
+namespace Backend_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
