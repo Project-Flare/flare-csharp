ebe7a65e5f9ea6c166e61b175329d6e1ad850444
herkusleonkaselis <159782252+herkusleonkaselis@users.noreply.github.com>
2024-05-14 15:25:19 +0300

    feat: protocol-compliant message encryption routines

--- flare-csharp/Crypto.cs
+++ flare-csharp/Crypto.cs
@@ -1,10 +1,24 @@
-﻿using Isopoh.Cryptography.Argon2;
+﻿using Google.Protobuf.WellKnownTypes;
+using Isopoh.Cryptography.Argon2;
 using Isopoh.Cryptography.SecureArray;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Utilities;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace flare_csharp
 {
+    public class FlareAeadCiphertext(byte[] ciphertext, byte[] nonce)
+    {
+        // Combined AEAD authentication tag and AES output
+        public byte[] Ciphertext { get; set; } = ciphertext;
+        public byte[] Nonce { get; set; } = nonce;
+    }
     public static class Crypto
     {
         /// <summary>
@@ -51,6 +65,50 @@ namespace flare_csharp
             {
                 cred.Argon2Hash = argonConfig.EncodeString(hash.Buffer);
             }
+        }
+
+        /// <summary>
+        /// Encrypts a byte array plaintext to the Flare protocol's transmission format
+        /// </summary>
+        /// <param name="key">32-byte encryption key</param>
+        /// <param name="plaintext">Variable-length input plaintext</param>
+        public static FlareAeadCiphertext FlareAeadEncrypt(byte[] key, byte[] plaintext)
+        {
+            KeyParameter keyParam = new(key);
+
+            IBlockCipher cipher = new AesEngine();
+            int macSize = 8 * cipher.GetBlockSize();
+            byte[] nonce = RandomNumberGenerator.GetBytes(12);
+            byte[] associatedText = [];
+            AeadParameters keyParamAead = new(keyParam, macSize, nonce, associatedText);
+            GcmSivBlockCipher cipherMode = new(cipher);
+            cipherMode.Init(true, keyParamAead);
+
+            int outputSize = cipherMode.GetOutputSize(plaintext.Length);
+            byte[] ciphertext = new byte[outputSize];
+            int result = cipherMode.ProcessBytes(plaintext, 0, plaintext.Length, ciphertext, 0);
+            cipherMode.DoFinal(ciphertext, result);
+            return new FlareAeadCiphertext(ciphertext, nonce);
+        }
+
+        public static byte[] FlareAeadDecrypt(byte[] key, FlareAeadCiphertext ciphertextPackage)
+        {
+            KeyParameter keyParam = new(key);
+            byte[] ciphertext = ciphertextPackage.Ciphertext;
+
+            IBlockCipher cipher = new AesEngine();
+            int macSize = 8 * cipher.GetBlockSize();
+            byte[] nonce = ciphertextPackage.Nonce;
+            byte[] associatedText = [];
+            AeadParameters keyParamAead = new(keyParam, macSize, nonce, associatedText);
+            GcmSivBlockCipher cipherMode = new(cipher);
+            cipherMode.Init(false, keyParamAead);
+
+            int outputSize = cipherMode.GetOutputSize(ciphertext.Length);
+            byte[] plainTextData = new byte[outputSize];
+            int result = cipherMode.ProcessBytes(ciphertext, 0, ciphertext.Length, plainTextData, 0);
+            cipherMode.DoFinal(plainTextData, result);
+            return plainTextData;
         }
     }
 }
